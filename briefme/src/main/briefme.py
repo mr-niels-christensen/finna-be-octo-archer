37358038c61b9cab3f5e07d04266b913f148e96a
Niels H. Christensen <nhc@mayacs.com>
2015-06-25 15:20:51 +0100

    More general search for relations. Though person-centered.

--- briefme/src/main/briefme.py
+++ briefme/src/main/briefme.py
@@ -1,25 +1,56 @@
 import rdflib
 from rdflib.namespace import DCTERMS, RDF
+from collections import Counter
+
+#s = rdflib.URIRef("http://dbpedia.org/resource/Margaret_Thatcher")
+s = rdflib.URIRef("http://dbpedia.org/resource/Augustus")
 
 g = rdflib.Graph()
-g.parse("http://dbpedia.org/resource/Margaret_Thatcher")
+g.parse(unicode(s))
 for o in g.objects(predicate = rdflib.URIRef("http://dbpedia.org/ontology/abstract")):
     if o.language == 'en':
         print o
-for p in [DCTERMS.subject, 
-          RDF.type, 
-          rdflib.URIRef('http://dbpedia.org/property/profession'),
-          rdflib.URIRef('http://dbpedia.org/property/office'),
-          rdflib.URIRef('http://dbpedia.org/property/spouse'),
-          rdflib.URIRef('http://dbpedia.org/property/children'),
+
+total = Counter()
+
+for p in [rdflib.URIRef('http://dbpedia.org/property/children'),
           rdflib.URIRef('http://dbpedia.org/property/predecessor'),
           rdflib.URIRef('http://dbpedia.org/property/successor'),
           rdflib.URIRef('http://dbpedia.org/property/deputy'),
-          rdflib.URIRef('http://dbpedia.org/property/spouse'),
-          rdflib.URIRef('http://dbpedia.org/property/wordnet_type')]:
-    print '-' * 40
-    print p
+          rdflib.URIRef('http://dbpedia.org/property/spouse')]:
+    #print '-' * 40
+    #print p
     for o in g.objects(rdflib.URIRef("http://dbpedia.org/resource/Margaret_Thatcher"), p):
-        print o
+        total[o] += 1
 
+#print total.most_common(10)
+print '.',
+
+for p in [DCTERMS.subject, 
+          #RDF.type, 
+          rdflib.URIRef('http://dbpedia.org/property/profession'),
+          rdflib.URIRef('http://dbpedia.org/property/office'),
+          rdflib.URIRef('http://dbpedia.org/property/wordnet_type')]:
+    #print '-' * 40
+    #print p
+    for o in g.objects(s, p):
+        #print u"--> {}".format(o)
+        print '.',
+        try:
+            gg = rdflib.Graph()
+            gg.parse(unicode(o))
+            part = Counter()
+            for oo in gg.subjects(p, o):
+                part[oo] += 1
+            #print sum(part.values())
+            total += part
+            #print sum(part.values())
+        except Exception as e:
+            print e
+        #print total.most_common(2)
+
+print
+
+for (uriref, no) in total.most_common(10):
+    print u'{}: {}'.format(no, unicode(uriref))
 
