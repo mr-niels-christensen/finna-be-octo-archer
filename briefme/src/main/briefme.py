5910e62d4564e5357123e145ec9adb7f1bf56e8b
Niels H. Christensen <nhc@mayacs.com>
2015-07-13 15:33:37 +0100

    Version running on GAE. Though I only get the abstract.

--- briefme/src/main/briefme.py
+++ briefme/src/main/briefme.py
@@ -5,67 +5,52 @@ from langdetect import detect
 
 import urllib2
 import json
-from sys import stdin
 
-print 'Who?'
-x = stdin.readline()
-req = urllib2.Request('http://lookup.dbpedia.org/api/search/KeywordSearch?MaxHits=1&QueryString={}'.format(x), None, {'Accept': 'application/json'})
-f = urllib2.urlopen(req)
-response = f.read()
-f.close()
-data = json.loads(response)
-#s = rdflib.URIRef("http://dbpedia.org/resource/Margaret_Thatcher")
-#s = rdflib.URIRef("http://dbpedia.org/resource/Augustus")
-s = rdflib.URIRef(data["results"][0]["uri"])
-g = rdflib.Graph()
-g.parse(unicode(s))
-abstracts = dict()
-for o in g.objects(predicate = rdflib.URIRef("http://dbpedia.org/ontology/abstract")):
-    print '{},{}'.format(o.language, detect(o))
-    abstracts[detect(o)] = o
-a = abstracts['en'] if 'en' in abstracts else abstracts[None]
-print a
+def brief(x, responder):
+    req = urllib2.Request('http://lookup.dbpedia.org/api/search/KeywordSearch?MaxHits=1&QueryString={}'.format(x), None, {'Accept': 'application/json'})
+    f = urllib2.urlopen(req)
+    response = f.read()
+    f.close()
+    data = json.loads(response)
+    #s = rdflib.URIRef("http://dbpedia.org/resource/Margaret_Thatcher")
+    #s = rdflib.URIRef("http://dbpedia.org/resource/Augustus")
+    s = rdflib.URIRef(data["results"][0]["uri"])
+    g = rdflib.Graph()
+    g.parse(unicode(s))
+    abstracts = dict()
+    for o in g.objects(predicate = rdflib.URIRef("http://dbpedia.org/ontology/abstract")):
+        print '{},{}'.format(o.language, detect(o))
+        abstracts[detect(o)] = o
+    a = abstracts['en'] if 'en' in abstracts else abstracts[None]
+    responder.writeln(a.encode('latin_1','ignore'))
 
-total = Counter()
+    total = Counter()
 
-for p in [rdflib.URIRef('http://dbpedia.org/property/children'),
-          rdflib.URIRef('http://dbpedia.org/property/predecessor'),
-          rdflib.URIRef('http://dbpedia.org/property/successor'),
-          rdflib.URIRef('http://dbpedia.org/property/deputy'),
-          rdflib.URIRef('http://dbpedia.org/property/spouse')]:
-    #print '-' * 40
-    #print p
-    for o in g.objects(rdflib.URIRef("http://dbpedia.org/resource/Margaret_Thatcher"), p):
-        total[o] += 1
+    for p in [rdflib.URIRef('http://dbpedia.org/property/children'),
+              rdflib.URIRef('http://dbpedia.org/property/predecessor'),
+              rdflib.URIRef('http://dbpedia.org/property/successor'),
+              rdflib.URIRef('http://dbpedia.org/property/deputy'),
+              rdflib.URIRef('http://dbpedia.org/property/spouse')]:
+        for o in g.objects(rdflib.URIRef("http://dbpedia.org/resource/Margaret_Thatcher"), p):
+            total[o] += 1
 
-#print total.most_common(10)
-print '.',
-
-for p in [DCTERMS.subject, 
-          #RDF.type, 
-          rdflib.URIRef('http://dbpedia.org/property/profession'),
-          rdflib.URIRef('http://dbpedia.org/property/office'),
-          rdflib.URIRef('http://dbpedia.org/property/wordnet_type')]:
-    #print '-' * 40
-    #print p
-    for o in g.objects(s, p):
-        #print u"--> {}".format(o)
-        print '.',
-        try:
-            gg = rdflib.Graph()
-            gg.parse(unicode(o))
-            part = Counter()
-            for oo in gg.subjects(p, o):
-                part[oo] += 1
-            #print sum(part.values())
-            total += part
-            #print sum(part.values())
-        except Exception as e:
-            print e
-        #print total.most_common(2)
-
-print
-
-for (uriref, no) in total.most_common(10):
-    print u'{}: {}'.format(no, unicode(uriref))
+    for p in [#DCTERMS.subject, 
+              #problematic RDF.type, 
+              #rdflib.URIRef('http://dbpedia.org/property/profession'),
+              rdflib.URIRef('http://dbpedia.org/property/office'),
+              #rdflib.URIRef('http://dbpedia.org/property/wordnet_type')
+              ]:
+        for o in g.objects(s, p):
+            try:
+                logging.info(unicode(o))
+                gg = rdflib.Graph()
+                gg.parse(unicode(o))
+                part = Counter()
+                for oo in gg.subjects(p, o):
+                    part[oo] += 1
+                total += part
+            except Exception as e:
+                print e
+    for (uriref, no) in total.most_common(10):
+        responder.writeln(u'{}: {}'.format(no, unicode(uriref)).encode('latin_1','ignore'))
 
---
+++ briefme/src/main/main.py
@@ -0,0 +1,73 @@
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.api import xmpp
+import time
+import logging
+
+import briefme
+
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            if users.is_current_user_admin():
+                self.response.out.write(
+                    'Hi Admin'
+                )
+            else:
+                self.response.out.write(
+                    'Hi non-admin {}. Nothing to see here.'.format(user.nickname())
+                )
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+
+def _src_dest_body(message):
+    return (message.sender.split('/')[0],
+            message.to,
+            message.body.encode('utf-8','ignore'))
+
+class ChatReceiver(webapp.RequestHandler):
+    def post(self):
+        message = xmpp.Message(self.request.POST)
+        src, dest, body = _src_dest_body(message)
+        trimmed_body = body.strip().lower().translate(None, '?!.,;:-')
+        logging.info("From <{}> to {}: <{}> -> <{}>".format(src, dest, body, trimmed_body))
+        respond(src, trimmed_body, Responder(message))
+        
+class Responder():
+    def __init__(self, message):
+        self._message = message
+        self._buffer = ''
+        
+    def write(self, text):
+        text = self._buffer + text
+        for raw_line in text.split('\n')[:-1]:
+            line = raw_line.strip()
+            if len(line) > 0:
+                self._send(line)
+        self._buffer = text.split('\n')[-1]
+    
+    def writeln(self, text):
+        text = self._buffer + text
+        for raw_line in text.split('\n'):
+            line = raw_line.strip()
+            if len(line) > 0:
+                self._send(line)
+        self._buffer = ''
+    
+    def _send(self, line):
+        time.sleep(float(len(line)) / 50)
+        logging.info("To <{orig[0]}> from {orig[1]}: <{line}>".format(line = line, orig = _src_dest_body(self._message)))
+        self._message.reply(line)
+
+def respond(sender, trimmed_body, responder):
+    briefme.brief(trimmed_body, responder)
+
+application = webapp.WSGIApplication([('/', MainPage), ('/_ah/xmpp/message/chat/', ChatReceiver)], debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
