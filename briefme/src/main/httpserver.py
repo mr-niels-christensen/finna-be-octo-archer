97accb31269b442dcb6f5a7f3d98a5b57b28f89a
Niels H. Christensen <nhc@mayacs.com>
2015-07-17 14:12:19 +0100

    Removed obsolete DBPedia lookup in Python

--- briefme/src/main/httpserver.py
+++ briefme/src/main/httpserver.py
@@ -10,23 +10,11 @@ import json
 import cache
 from google.appengine.api import taskqueue
 
-def lookup_route():
-    return webapp2.Route(r'/lookup/<phrase>', handler=_LookupHandler, name='lookup')
-
 def content_route():
     return webapp2.Route(r'/content', handler=_ContentHandler, name='content')
 
 def add_route():
     return webapp2.Route(r'/add', handler=_AddHandler, name='add')
-
-class _LookupHandler(webapp2.RequestHandler):
-    def get(self, phrase):
-        #Access-Control-Allow-Origin: *
-        self.response.headers['Access-Control-Allow-Origin'] = '*'
-        self.response.headers['Content-Type'] = 'application/json; charset=utf-8'
-        result = selectresource.lookup(phrase)
-        result = json.dumps(result)
-        self.response.write(result)    
 
 class _ContentHandler(webapp2.RequestHandler):
     def get(self):
@@ -64,7 +52,6 @@ class MainPage(webapp2.RequestHandler):
             self.redirect(users.create_login_url(self.request.uri))
 
 application = webapp2.WSGIApplication([
-    lookup_route(),
     content_route(),
     add_route(),
 ], debug=True) #debug=true means stack traces in browserapplication = webapp.WSGIApplication([('/', MainPage), ('/_ah/xmpp/message/chat/', ChatReceiver)], debug=True)
