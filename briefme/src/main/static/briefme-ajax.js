aca824de20cfc5579fe5e990109e27446bf6c684
Niels H. Christensen <nhc@mayacs.com>
2015-07-22 13:23:31 +0100

    Document JS

--- briefme/src/main/static/briefme-ajax.js
+++ briefme/src/main/static/briefme-ajax.js
@@ -1,14 +1,19 @@
+//Common abbrieviation and optimization
 $body = $("body");
 
+//Hook the lookup form up to the lookup() function
 $( "#lookup" ).submit(function( event ) {
   event.preventDefault();
   lookup();
 });
 
+//Use string from #search to search DBpedia for resources
+//List found resources in the table in #options
 function lookup() {
 	$( "#options" ).empty();
 	$( "#show" ).empty();
 	var search_for = $( '#search' ).val();
+	//TODO: Handle failures
 	$.ajax({
     url: "http://lookup.dbpedia.org/api/search/KeywordSearch",
     data: {
@@ -22,8 +27,10 @@ function lookup() {
     timeout: 4000,
   });
 }
+
+//Add an individual DBpedia resource to the table in #options
 function _add_result( index, result ) {
-	var content_uri = '/get-item/dbpedia-resource/'+encodeURIComponent(result.uri.split("/").pop());
+	var content_uri = '/get-item/dbpedia-resource/' + encodeURIComponent(result.uri.split("/").pop());
 	$( "#options" ).append( "<tr></tr>" );
 	$( "#options tr:last" ).on( "click", function() {
   	  show(content_uri);
@@ -33,14 +40,20 @@ function _add_result( index, result ) {
 	$( "#options tr:last" ).append( "<td><small></small></td>" );
 	$( "#options tr:last small" ).append( result.description );
 }
+
+//Request abstracts for a DBpedia resource,
+//then wait for them to be ready (updating progress bar),
+//then display them on #show
 function show(uri){
 	$body.addClass("working");
+	//TODO: Handle failures, consider setInterval() and clearInterval() or similar from jquery
 	$.ajax({
     url: uri,
     dataType: 'json',
 	success: function( response ) {
 				if (response.ready) {
 					$body.removeClass("working");
+					reset_progress_bar();
 	      			$( "#show" ).empty();
 	      			$.each( response.data, _show_abstract );
 				} else {
@@ -51,6 +64,8 @@ function show(uri){
     timeout: 1500,
   });	
 }
+
+//Add an individual abstract to #show
 function _show_abstract( index, abstract ) {
 	$( "#show" ).append( "<p>" + abstract + "</p>" )
 }
--- briefme/src/main/static/progress.js
+++ briefme/src/main/static/progress.js
@@ -1,4 +1,7 @@
-function reset() {
+//TODO: Give the progress bar variable size, it's fixed to 200px right now
+
+//Put the progress bar back to 0%
+function reset_progress_bar() {
 	document.getElementById("mask").style.left = "0px";
 	document.getElementById("mask").style.width = document.getElementById("progressbar").offsetWidth + "px";
 	document.getElementById("progressIndicator").style.zIndex  = 10;
@@ -6,6 +9,7 @@ function reset() {
 	document.getElementById("progressIndicator").innerHTML = "0%";
 }
 
+//Set the progress bar to the given fraction between 0 and 1
 function setProgress(fraction) {
 	document.getElementById("progressIndicator").innerHTML = Math.floor(fraction*100) + "%";
 	if(fraction > 0.99) {
