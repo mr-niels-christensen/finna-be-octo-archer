89eed76926ede782080820cdf4ed853c5b5003f4
Niels H. Christensen <nhc@mayacs.com>
2015-07-30 14:43:38 +0100

    Improve speak, and make it stop. Sometime.

--- briefme/src/main/static/player.js
+++ briefme/src/main/static/player.js
@@ -5,6 +5,8 @@
  * @param item {string} The item to play, e.g. 'Mozart'.
  */
 function _play_item(show, item) {
+  //Always cancel any ongoing utterances on update
+  window.speechSynthesis.cancel();
   //Check show, return if not == 'player'
   if (show != 'player') {
     return;
@@ -17,11 +19,15 @@ function _play_item(show, item) {
 	success: function (response) {
 		$.each(response.data, function (index, label_or_abstract){
 			//TODO: Allow pause/stop
-      if (index % 2 == 0) {
+      if (index % 2 == 0) {//A label
         $( '#canvas' ).append( '<p></p>' );
-        $( '#canvas p:last' ).html( label_or_abstract );        
+        $( '#canvas p:last' ).html( label_or_abstract );
+        if (label_or_abstract.length > 0) {
+          _narrate_text('Next up: ' + label_or_abstract, true);
+        }
+      } else {//An abstract
+        _narrate_text(label_or_abstract, false);
       }
-			_narrate_text(label_or_abstract);
 		})
 	},
 	timeout: 2500,
@@ -31,22 +37,31 @@ function _play_item(show, item) {
 /**
  * Narrates the given text.
  * @param txt {string} A string of sentences, delimited by '.'s
+ * @param high_pitch {boolean} If true, the pitch of the voice will
+ * have above-average pitch.
  */
-function _narrate_text(txt) {
+function _narrate_text(txt, high_pitch) {
   //See http://updates.html5rocks.com/2014/01/Web-apps-that-talk---Introduction-to-the-Speech-Synthesis-API
   //Dodge https://code.google.com/p/chromium/issues/detail?id=369472
   var txts = txt.split('.');
   $.each(txts, function( index, value ) {
-  	if (value.length > 349) {//TODO: Handle this
+    if (value.length > 349) {//TODO: Handle this
         console.log(index + ': Too long, ' + value.length);
-   		return;
-  	}
+      return;
+    }
+    if (value.length < 2) {
+        console.log(index + ': Too short, ' + value);
+      return;
+    }
     var msg = new SpeechSynthesisUtterance(value);
+    if (high_pitch) {
+      msg.pitch = 1.5;
+    }
     msg.onend = function(event) {
       console.log(index + ': Finished');
     };
     window.speechSynthesis.speak(msg);    
-  })  
+  })
 }
 
 /** Call _play_item() when appstate changes */
