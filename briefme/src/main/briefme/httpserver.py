580a9d91839ab832e15f6c122767ad0a2fd1aa0f
Niels H. Christensen <nhc@mayacs.com>
2015-08-03 14:45:07 +0100

    Remove obsolete comment

--- briefme/src/main/briefme/httpserver.py
+++ briefme/src/main/briefme/httpserver.py
@@ -81,7 +81,6 @@ class _AddToChannelHandler(webapp2.RequestHandler):
 #TODO better naming of URLs
 application = webapp2.WSGIApplication([
     webapp2.Route(r'/get-item/dbpedia-resource/<name>', handler=_GetItemDbpediaResourceHandler, name='get-item'),
-    #TODO: Implement the below properly or remove it
     webapp2.Route(r'/create-item', handler=_CreateItemHandler, name='create-item'),
     webapp2.Route(r'/get-channel', handler=_GetChannelHandler, name='get-channel'),
     webapp2.Route(r'/add-to-feed/<name>', handler=_AddToChannelHandler, name='add-to-channel'),
