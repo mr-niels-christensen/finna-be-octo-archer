feb880744fd0e853fc4854c6763c28068afa88f0
Niels H. Christensen <nhc@mayacs.com>
2015-08-06 11:35:28 +0100

    Do not retry when failing to load e.g. the graph of all Living People

--- briefme/src/main/briefme/webgraph.py
+++ briefme/src/main/briefme/webgraph.py
@@ -9,11 +9,20 @@ import rdflib
 class UnavailableError(Exception):
     pass
 
+class LargeGraphOrSlowConnectionError(Exception):
+    pass
+
 class WebGraph(rdflib.Graph):
-    def __init__(self, uri):
+    def __init__(self, uri, fail_if_not_loaded = False):
         super(WebGraph, self).__init__()
         uri = unicode(uri)
-        self.parse(format = 'n3', data = _get_uri(uri))
+        try:
+            self.parse(format = 'n3', data = _get_uri(uri))
+        except LargeGraphOrSlowConnectionError as e:
+            if fail_if_not_loaded:
+                raise e
+            else:
+                logging.debug('Large graph or slow connetion for {}'.format(uri), exc_info = True);
 
 def _get_uri(uri):
     result = memcache.get('uri ' + uri)
@@ -28,7 +37,7 @@ def _load(uri):
                                   headers = {'Accept' : 'text/rdf+n3'},
                                   deadline = 20)
     except DeadlineExceededError:
-        raise UnavailableError('The external database is currently unavailable, deadline exceeded.')
+        raise LargeGraphOrSlowConnectionError('The external database is currently unavailable, deadline exceeded.')
     if response.status_code == 502:
         raise UnavailableError('The external database is currently unavailable, error 502.')
     if response.status_code != 200:
