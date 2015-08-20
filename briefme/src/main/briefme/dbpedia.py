0ce2390661838f4f6ab3bd022fedfe947f2b5254
Niels H. Christensen <nhc@mayacs.com>
2015-08-20 15:14:41 +0100

    Remove unused triples

--- briefme/src/main/briefme/dbpedia.py
+++ briefme/src/main/briefme/dbpedia.py
@@ -4,31 +4,57 @@ from rdflib import namespace
 class DBpediaResource(object):
     def __init__(self):
         self._graph = rdflib.Graph()
-        self._graph.bind('OWL', namespace.OWL)
+        self._graph.bind('OWL', 'http://www.w3.org/2002/07/owl#')
         self._graph.bind('FOAF', namespace.FOAF)
         self._graph.bind('DC', namespace.DC)
         self._graph.bind('DCTERMS', namespace.DCTERMS)
         self._graph.bind('PROP', 'http://dbpedia.org/property/')
         self._graph.bind('RES', 'http://dbpedia.org/resource/')
         self._graph.bind('ONT', 'http://dbpedia.org/ontology/')
+        self._graph.bind('PROV', 'http://www.w3.org/ns/prov#')
 
     @staticmethod
-    def for_name(name):
+    def for_name(name, lang='en'):
         url = 'http://dbpedia.org/resource/{}'.format(name)
         res = DBpediaResource()
         res._graph.bind('I', url)
         res._graph.parse(url)
+        res._rm_other_languages(lang)
+        res._rm_admin_triples()
         return res
 
-    def _get(self, predicate_qname):
-        (pref, name) = predicate_qname.split(':')
-        qname = None
+    def _rm_other_languages(self,lang = 'en'):
+        for (s, p, o) in self._graph:
+            if isinstance(o, rdflib.Literal):
+                if o.language not in [lang, None]:
+                    self._graph.remove((s, p, o))
+
+    def _rm_admin_triples(self):
+        for qname in ['ONT:wikiPageDisambiguates', 
+                      'ONT:wikiPageRedirects', 
+                      'ONT:wikiPageID', 
+                      'ONT:wikiPageRevisionID', 
+                      'OWL:differentFrom', 
+                      'OWL:sameAs', 
+                      'FOAF:primaryTopic' ,
+                      'FOAF:isPrimaryTopicOf', 
+                      'PROV:wasDerivedFrom']:
+            p = self._to_uriref(qname)
+            print p
+            for (s, o) in self._graph[:p]:
+                self._graph.remove((s, p, o))
+
+    def _to_uriref(self, qname):
+        (pref, name) = qname.split(':')
         for prefix, namespace in self._graph.namespaces():
             if pref == prefix:
-                for (s,o) in self._graph[:rdflib.URIRef(name, base = namespace)]:
-                    yield (self._graph.qname(s), self._graph.qname(o))
-                return
-        raise Exception('{} not recognized as a prefix, in {}'.format(pref, predicate_qname))
+                return rdflib.URIRef(unicode(namespace) + unicode(name))
+        raise Exception('{} not recognized as a prefix, in {}'.format(pref, qname))
+
+
+    def _get(self, predicate_qname):
+        for (s,o) in self._graph[:self._to_uriref(predicate_qname)]:
+            yield (self._graph.qname(s), self._graph.qname(o))
 
 if __name__ == '__main__':
     res = DBpediaResource.for_name('Titan_(moon)')
